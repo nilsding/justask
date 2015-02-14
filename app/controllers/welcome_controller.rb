1dc783caf77af83c125b1c5fc90652ffb7d5aff3
Andreas N <pixeldesu@nerdwerks.org>
2015-02-14 19:29:23 +0100

    add controller for welcome pages

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,25 @@
+class WelcomeController < ApplicationController
+  def show
+
+  end
+
+  def inbox
+
+  end
+
+  def answer
+
+  end
+
+  def profile
+
+  end
+
+  def services
+
+  end
+
+  def finish
+
+  end
+end
